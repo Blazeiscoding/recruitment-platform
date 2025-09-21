a93ffe9b230dd95406f91d1365c3ca435cf8b4c0
Nikhil Rathore <nikhilrathore52297@gmail.com>
2025-09-21 22:15:28 +0530

    Initiated project

---
+++ utils/database.js
@@ -0,0 +1,21 @@
+const { Pool } = require("pg");
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl:
+    process.env.NODE_ENV === "production"
+      ? { rejectUnauthorized: false }
+      : false,
+});
+
+// Test connection
+pool.connect((err, client, release) => {
+  if (err) {
+    console.error("Error acquiring client", err.stack);
+    return;
+  }
+  console.log("Database connected successfully");
+  release();
+});
+
+module.exports = pool;
